5c5e91fac76785366c2aec94cbb5b2a76e930a90
Neverlink <neverlink1337@gmail.com>
2018-08-01 17:23:30 +0200

    Start working

---
+++ idfinder/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace idfinder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(703, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Find IDs!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(283, 259);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(245, 38);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Words",
+            "Letters",
+            "Numbers"});
+            this.comboBox1.Location = new System.Drawing.Point(545, 259);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 37);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Letters";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1197, 593);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "MainForm";
+            this.Text = "id-finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ idfinder/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using PortableSteam;
+using System.IO;
+using idfinder.Properties;
+
+namespace idfinder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SteamWebAPI.SetGlobalKey(Resources.api);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string method = comboBox1.Text;
+            int count = (int)numericUpDown1.Value;
+            List<string> ids = new List<string>();
+
+            switch (method)
+            {
+                case "Word":
+                    break;
+                case "Letters":
+                    break;
+                case "Numbers":
+                    break;
+                default:
+                    MessageBox.Show("Invalid method!");
+                    return;
+            }
+            //var result = SteamWebAPI.General().ISteamUser().ResolveVanityURL("kneecocks").GetResponse();
+        }
+    }
+    //class Student
+    //{
+    //    public string Name { get; set; }
+    //    public int Age { get; set; }
+    //    public double AvgGrade { get; set; }
+
+    //    public void SayDespacito()
+    //    {
+    //        MessageBox.Show(this.Name + " says despacito");
+
+    //    }
+    //    public Student(string name, int age, double avggrade)
+    //    {
+    //        this.Name = name;
+    //        this.Age = age;
+    //        this.AvgGrade = avggrade;
+    //    }
+    //}
+}
